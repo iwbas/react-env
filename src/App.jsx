76093f9c189aa8fc291d74f26f77b575e8acf046
iwbas <brexceed@gmail.com>
2021-06-24 18:19:57 +0400

    asdas

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,18 @@
 
 */
 
-import "react-app-polyfill/ie11";
-import { useState, useRef } from "react";
+import 'react-app-polyfill/ie11';
+import { useState, useRef } from 'react';
 
-import "./App.css";
-import EditorTab from "./components/EditorTab";
+import './App.css';
+import EditorTab from './components/EditorTab';
 // import { ManagmentPanel } from "./components/ManagmentPanel";
-import codeTemplate from "./utils/code_template";
-import { loader } from "@monaco-editor/react";
+import codeTemplate from './utils/code_template';
+import { loader } from '@monaco-editor/react';
 
 loader.config({
   paths: {
-    vs: "/vs",
+    vs: '/vs',
   },
 });
 
@@ -24,32 +24,46 @@ function App() {
   const [isLoaded, setIsLoaded] = useState(true);
 
   const [codeValue, setCodeValue] = useState(codeTemplate);
-  const [inputValue, setInputValue] = useState("");
-  const [outputValue, setOutputValue] = useState("");
+  const [inputValue, setInputValue] = useState('');
+  const [outputValue, setOutputValue] = useState('');
 
   const run = async () => {
-    setIsLoaded(false);
-    const url = "http://localhost:8080/run";
+    const url = 'http://194.233.160.8/api/run';
     const data = { code: codeValue, stdin: inputValue };
-    try {
-      const response = await fetch(url, {
-        method: "POST", // или 'PUT'
-        body: JSON.stringify(data), // данные могут быть 'строкой' или {объектом}!
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      const json = await response.json();
-      console.log(json.output);
-      console.log("Успех:", JSON.stringify(json));
-      // setOutputValue(jsFriendlyJSONStringify(json.output));
-      setOutputValue(json.output);
-    } catch (error) {
-      console.error("Ошибка:", error);
-      setError(error);
-    }
-    setIsLoaded(true);
+    const response = fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then((response) => response.json().then(json => {
+      setOutputValue(json.output)
+    }));
   };
+
+  // const run = async () => {
+  //   setIsLoaded(false);
+  //   const url = "http://localhost:8080/run";
+  //   const data = { code: codeValue, stdin: inputValue };
+  //   try {
+  //     const response = await fetch(url, {
+  //       method: "POST", // или 'PUT'
+  //       body: JSON.stringify(data), // данные могут быть 'строкой' или {объектом}!
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //     });
+  //     const json = await response.json();
+  //     console.log(json.output);
+  //     console.log("Успех:", JSON.stringify(json));
+  //     // setOutputValue(jsFriendlyJSONStringify(json.output));
+  //     setOutputValue(json.output);
+  //   } catch (error) {
+  //     console.error("Ошибка:", error);
+  //     setError(error);
+  //   }
+  //   setIsLoaded(true);
+  // };
 
   if (error) return <div>Ошибка: {error.message}</div>;
 
